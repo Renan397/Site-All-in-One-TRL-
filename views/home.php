5cec8520edd2af6ccf692cd259da1bf4a1993e91
Renan397 <renan.santos@aluno.ifsp.edu.br>
2021-11-19 11:38:27 -0300

    test10

--- views/home.php
+++ views/home.php
@@ -29,8 +29,8 @@
 
 <div id="texto_entrada">
 <h3 class="titulo_entrada">Bem-Vindo!</h3>
-<p class="texto_entrada">Em breve, a TRL Sites estará desenvolvendo o website ideal para você e o seu negócio! Oi Renan!!!</p>
-<p>Oi</p>
+<p class="texto_entrada">Em breve, a TRL Sites estará desenvolvendo o website ideal para você e o seu negócio! Oi Renan vagabudno!!!</p>
+<p>Oi oi</p>
 <img id="img_maozinha" src="<?=$img_files[1]?>" width = "80px"; height = "60px";>
 </div>
 
