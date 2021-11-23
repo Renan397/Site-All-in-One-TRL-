322bb091c348aa314089721dbe1d56810ccc1ec2
Renan397 <renan.santos@aluno.ifsp.edu.br>
2021-11-23 13:46:17 -0300

    teste

--- assets/js/home.js
+++ assets/js/home.js
@@ -1558,7 +1558,7 @@ $(function() {
     $('#slides_servicos figure').cycle({
         fx: 'cover',
 			speed: 1000,
-			timeout: 11000,
+			timeout: 10000,
 			next: '#prox',
 			prev: '#ant',
 			pager: '#pager'
