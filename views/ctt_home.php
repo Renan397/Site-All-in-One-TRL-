d77b74d2873d424c5d3c096f94f5540bd3eb56a5
ThiPM <thiago.martins@aluno.ifsp.edu.br>
2021-11-26 14:04:57 -0300

    back

--- views/ctt_home.php
+++ views/ctt_home.php
@@ -1,15 +1,15 @@
 <form method="POST" action="./Source/Database/query.php" class="row g-3">
   <div class="col-md-6">
     <label class="form-label">Nome:</label>
-    <input type="text" name="name" placeholder="Digite aqui o seu nome.." id="campo_nome" onfocus="clicou_campo_nome()" onblur="nao_clicou_campo_nome()" class="form-control">
+    <input type="text" name="name" required="required" placeholder="Digite aqui o seu nome.." id="campo_nome" onfocus="clicou_campo_nome()" onblur="nao_clicou_campo_nome()" class="form-control">
   </div>
   <div class="col-md-6">
     <label class="form-label">Email:</label>
-    <input type="email" name="email" placeholder="Digite aqui o seu email..." id="campo_email" onfocus="clicou_campo_email()" onblur="nao_clicou_campo_email()" class="form-control">
+    <input type="email" name="email" required="required" placeholder="Digite aqui o seu email..." id="campo_email" onfocus="clicou_campo_email()" onblur="nao_clicou_campo_email()" class="form-control">
   </div><br><br><br><br>
   <div class="col-md-6">
     <label class="form-label">Mensagem:</label>
-    <textarea type="text" maxlength="1000" minlength="1" name="message" style="height: 150px;" placeholder="Digite aqui sua mensagem..." id="campo_msg" onfocus="clicou_campo_msg()" onblur="nao_clicou_campo_msg()" class="form-control"></textarea>
+    <textarea type="text" maxlength="1000" required="required" minlength="1" name="message" style="height: 150px;" placeholder="Digite aqui sua mensagem..." id="campo_msg" onfocus="clicou_campo_msg()" onblur="nao_clicou_campo_msg()" class="form-control"></textarea>
   </div>
   <div class="col-12">
   <br><center><button type="submit" id="bttn_enviar" class="btn btn-primary">Enviar</button></center><br><br>
