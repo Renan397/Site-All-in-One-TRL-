f9f61cbdd2f25351cc9cc3c385cc8b9155f95e35
Renan397 <renan.santos@aluno.ifsp.edu.br>
2022-12-06 22:15:54 -0300

    Update Title

--- views/home.php
+++ views/home.php
@@ -22,7 +22,7 @@
   $img_files[15] = $img."logo_gmail_serv.png";
   ?>
 <head>
-<title>TRL Sites</title>
+<title>TRL Sites - O seu site, a sua evolução!</title>
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <!-- jQuery Bootstrap -->
